c222c1b266ec5ba5000556d0c7e7b72709539d53
ajaycprakash <ajaycprakash18@gmail.com>
2024-03-18 14:30:57 +0530

    fizz

---
+++ fizz-buzz.js
@@ -0,0 +1,28 @@
+function myFunction(){
+    var num = document.getElementById("fizz-buzz").value;
+    var result=document.getElementById("fizzbuzz_result");
+    result.innerHTML="";
+for (var i = 1; i<=num; i++) {
+    var item = document.createElement("li");
+
+    if (i % 15 ==0&&i%5==0) {
+        item.textContent="FizzBuzz"
+    }
+    
+    else if (i % 3 == 0) {
+        item.textContent="Fizz"
+
+    }
+
+    else if (i % 5 == 0) {
+        item.textContent="FizzBuzz"
+    }
+    else {
+       item.textContent=i;
+}
+
+
+result.appendChild(item);
+}
+
+}
